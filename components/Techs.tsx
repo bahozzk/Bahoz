dbfb4ef217e00d440a33be984648a76377b9a928
Bahoz <73389375+bahozzk@users.noreply.github.com>
2023-03-10 20:46:48 +0300

    Düzenleme

--- components/Techs.tsx
+++ components/Techs.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Minecraft } from '@fortawesome/free-brands-svg-icons';
+import { faUbuntu } from '@fortawesome/free-brands-svg-icons';
 import type { NextPage } from 'next';
 import type { PageProps } from '../types';
 
@@ -7,14 +7,14 @@ const Techs: NextPage = (pageProps: PageProps): JSX.Element => {
   const techs = [
     {
       //Minecraft Evi
-      icon: 'https://media.discordapp.net/attachments/1077331799262363649/1083800777799249930/minecraftevi.png'
       href: 'https://youtube.com/minecraftevi'
+      icon: 'https://media.discordapp.net/attachments/1077331799262363649/1083800777799249930/minecraftevi.png'
     }
   ]
 
   return (
     <div className='my-20 md:px-8 w-full flex flex-col items-center'>
-      <h2 className='font-bold text-lg uppercase'>Technologies</h2>
+      <h2 className='font-bold text-lg uppercase'>Ekstralar</h2>
       <div className='flex flex-row flex-wrap items-center justify-center max-w-3xl'>
         {techs.map((data, index) => <a target={'_blank'} rel="noreferrer" href={data.href} className={`m-6 flex flex-row items-center`} key={`techs${index}`}>
           {data.icon}
